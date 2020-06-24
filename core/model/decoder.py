aecdeb7f110c2bfeebefe8b794a7e61be04857e7
lujie <relu@lujies-Mac-mini.local>
2020-06-24 20:15:05 +0800

    LGSC-for-FAS | Pytorch-version | build 40%

--- core/model/decoder.py
+++ core/model/decoder.py
@@ -3,40 +3,70 @@
 
 import torch
 import torch.nn as nn
-from .encoder import BasicBlock
+from torch.nn import functional as F
+from basemodule import BasicBlock, _make_res_layer
 from IPython import embed
 
-class Decoder(nn.Module):
+
+class DeCoder(nn.Module):
 
     def __init__(self):
 
         super(DeCoder, self).__init__()
         self.in_channels  = (64, 64, 128, 256, 512)
         self.out_channels = (512, 256, 128, 64, 64, 3)
-        self.layer1 = self._make_layer(BasicBlock, 512, 256, 2)
-        self.layer2 = self._make_layer(BasicBlock, 256, 128, 2)
-        self.layer3 = self._make_layer(BasicBlock, 128, 64,  2)
-        self.layer4 = self._make_layer(BasicBlock, 128, 64,  2)
-        self.layer5 = self._make_layer(BasicBlock, 64,  3,)
-        
+        self.res_layers   = []
+        self.conv1x1      = []
+        self.conv2x2      = []
+        self._make_layers()
 
+    def _make_layers(self):
 
-    def _make_layer(self, block, inplanes, planes, blocks, stride=1):
+        for i in range(len(self.in_channels)-1, -1, -1):
 
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
+            res_layer = _make_res_layer(
+                            BasicBlock,
+                            inplanes=128 if i == 1 else self.in_channels[i],
+                            outplanes=self.out_channels[-(i+1)],
+                            num_blocks=2,
+                            norm_layer=nn.InstanceNorm2d)
 
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            out_planes = self.in_channels[i] if i < 2 else int(self.in_channels[i] / 2)
+            conv2x2 = nn.Sequential(
+                          nn.Conv2d(self.in_channels[i], out_planes, kernel_size=2, bias=False),
+                          nn.InstanceNorm2d(out_planes),
+                          nn.ReLU(inplace=True))
 
-        return nn.Sequential(*layers)
+            conv1x1 = nn.Sequential(
+                          nn.Conv2d(in_channels  = 128 if i == 1 else self.in_channels[i],
+                                    out_channels = self.out_channels[-(i+1)],
+                                    kernel_size=1,
+                                    bias=False),
+                          nn.InstanceNorm2d(out_planes))
+            self.res_layers.append(res_layer)
+            self.conv2x2.append(conv2x2)
+            self.conv1x1.append(conv1x1)
+        self.res_layers = nn.ModuleList(self.res_layers)
+        self.conv2x2   = nn.ModuleList(self.conv2x2)
+        self.conv1x1   = nn.ModuleList(self.conv1x1)
 
+    def forward(self, x):
 
-    def _gen_layers(self):
+        assert len(x) == len(self.in_channels)
+
+        out  = x[-1]
+        outs = []
+        outs.append(out)
+
+        for i in range(len(self.in_channels)):
+
+            out = F.interpolate(out, scale_factor=2, mode='nearest')
+            out = F.pad(out, [0, 1, 0, 1])
+            out = self.conv2x2[i](out)
+            if i < 4:
+                out = torch.cat([out, x[-(i+2)]], dim=1)
+            identity = self.conv1x1[i](out)
+            out = self.res_layers[i](out) + identity
+            outs.append(out)
+        outs[-1] = torch.tanh(outs[-1])
+        return outs
--- core/model/encoder.py
+++ core/model/encoder.py
@@ -3,109 +3,72 @@
 
 import torch
 import torch.nn as nn
+from torchvision import models
+
 from IPython import embed
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-class BasicBlock(nn.Module):
-    expansion = 1
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1   = nn.BatchNorm2d(planes)
-        self.relu  = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2   = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
+# source-code : https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 
 class EnCoder(nn.Module):
 
-    def __init__(self, block = BasicBlock, layers = [2, 2, 2, 2]):
+    def __init__(self, pretrained = True):
 
         super(EnCoder, self).__init__()
+        self.backbone = models.resnet18(pretrained=pretrained)
+        self._freeze_model()
 
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False) # TODO
-        self.bn1   = nn.BatchNorm2d(self.inplanes)
-        self.relu  = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64,  layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
+    def _freeze_model(self):
+        for p in self.backbone.parameters():
+            p.required_grad = False
 
     def forward(self, x):
-
         outs = []
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
         outs.append(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
+        x = self.backbone.maxpool(x)
+        x = self.backbone.layer1(x)
         outs.append(x)
-        x = self.layer2(x)
+        x = self.backbone.layer2(x)
         outs.append(x)
-        x = self.layer3(x)
+        x = self.backbone.layer3(x)
         outs.append(x)
-        x = self.layer4(x)
+        x = self.backbone.layer4(x)
         outs.append(x)
         return tuple(outs)
 
+
+class Classifier(nn.Module):
+
+    def __init__(self, num_classes = 2, drop_ratio = 0.5, pretrained = True):
+
+        super(Classifier, self).__init__()
+        self.resnet18 = models.resnet18(pretrained=pretrained)
+        self.resnet18.fc = nn.Linear(self.resnet18.fc.in_features, num_classes)
+        self.dropout  = nn.Dropout(drop_ratio)
+
+        self._freeze_backbone()
+
+    def _freeze_backbone(self):
+        for p in self.resnet18.parameters():
+            p.required_grad = False
+        for p in self.resnet18.fc.parameters():
+            p.required_grad = True
+
+    def forward(self, x):
+        x = self.resnet18.conv1(x)
+        x = self.resnet18.bn1(x)
+        x = self.resnet18.relu(x)
+        x = self.resnet18.layer1(x)
+        x = self.resnet18.layer2(x)
+        x = self.resnet18.layer3(x)
+        x = self.resnet18.layer4(x)
+        x = self.resnet18.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.resnet18.fc(x)
+        return x
 
 if __name__ == "__main__":
 
